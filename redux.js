e429ac535902d37e86bcc2b531b21e67021001f5
Benjamin Huang <bhuang@rms.com>
2018-08-19 19:21:31 -0700

    add reduxt

--- redux.js
+++ redux.js
@@ -1,24 +1,26 @@
+/*
+    From action.js
+*/
 const merge = (prev, next) => {
     return Object.assign({}, prev, next)
 }
 
-const contactReducer = (state, newContact) => {
-    return [...state, newContact];
-}
-
-const userReducer = (state, update) => {
-    return merge(state, update);
-}
-
-const reducer = (state, action) => {
-    if (action.type === 'UPDATE_USER') {
-        return merge(state, { user: userReducer(state.user, action.payload) });
-    }
-    else if (action.type === 'UPDATE_CONTACT') {
-        return merge(state, { contacts: contactReducer(state.contacts, action.payload) });
-    }
+const contactReducer = (state, action) => {
+    if(action.type === 'UPDATE_CONTACT')
+        return [...state, action.payload];
     return state;
 }
+
+const userReducer = (state, action) => {
+    if(action.type === 'UPDATE_USER')
+        return merge(state, action.payload);
+    return state;
+}
+
+const reducer = (state, action) => ({
+   user: userReducer(state.user, action),
+   contacts: contactReducer(state.contacts, action)
+})
 
 class Store {
     constructor(reducer, initState) {
@@ -44,16 +46,11 @@ const DEFATUL_STATE = {
 const store = new Store(reducer, DEFATUL_STATE);
 console.log(store.getState());
 
-// store.dispatch({ type: 'UPDATE_USER', payload: { bar: 'bar' } });
-// Using cation creator
 const updateUser = update => ({
     type: 'UPDATE_USER',
     payload: update
 })
 store.dispatch(updateUser({ foo: "foo" }));
-
-// Using cation creator
-//store.dispatch({ type: 'UPDATE_CONTACT', payload: { name: 'ben', number:'123' }});
 
 const addCountact = newContact => ({
     type: 'UPDATE_CONTACT',
